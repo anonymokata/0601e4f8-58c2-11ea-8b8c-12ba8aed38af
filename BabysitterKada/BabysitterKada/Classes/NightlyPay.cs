a2997661cf74833c902bf92316a62d5f09a65989
someone <someone>
2020-02-22 10:03:49 -0500

    Added gitignore and removed ignored files

---
+++ BabysitterKada/BabysitterKada/Classes/NightlyPay.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BabysitterKada.Classes
+{
+    public class NightlyPay
+    {
+        public double CalculatePay(string startTime, string EndTime, double hourlyWage)
+        {
+            return 90.0;
+        }
+    }
+}
---
+++ BabysitterKada/BabysitterKadaTests/NightlyPayTest.cs
@@ -0,0 +1,17 @@
+﻿using BabysitterKada.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BabysitterKadaTests
+{
+    [TestClass]
+    public class NightlyPayTest
+    {
+        [TestMethod]
+        public void whenCalculatePayIsGivenSingleRateAndTimesItReturnsDollarsAsDouble()
+        {
+            NightlyPay nightlyPay = new NightlyPay();
+            Assert.AreEqual(nightlyPay.CalculatePay("5:00PM", "11:00PM", 15), 90.0);
+        }
+    }
+}
