516caf739a44970ac0fc697e884d72273ca82448
someone <someone>
2020-02-24 13:00:52 -0500

    Playing /w public api. Fixed some typos so familyA, B, and C have correct values.

--- BabysitterKada/BabysitterKada/Classes/FamilyB.cs
+++ BabysitterKada/BabysitterKada/Classes/FamilyB.cs
@@ -6,7 +6,7 @@ namespace BabysitterKada.Classes
 {
     public class FamilyB : Family
     {
-      public FamilyB() : base(10, 12, 16, "10:00PM", "12:00PM")
+      public FamilyB() : base(12, 8, 16, "10:00PM", "12:00AM")
         {
 
         }
--- BabysitterKada/BabysitterKada/Program.cs
+++ BabysitterKada/BabysitterKada/Program.cs
@@ -7,7 +7,22 @@ namespace BabysitterKada
     {
         static void Main(string[] args)
         {
-           Night night = new Night("12:00AM", "12:00PM");
+            Night night = new Night("6:00PM", "11:00PM");
+            Family family = new FamilyA();
+            PayCalculator calc = new PayCalculator(family, night);
+            Console.WriteLine(calc.CalculatePay());
+
+            Night night2 = new Night("7:00PM", "1:00AM");
+            Family familyB = new FamilyB();
+            PayCalculator calcB = new PayCalculator(familyB, night2);
+            Console.WriteLine(calcB.CalculatePay());
+
+            Night night3 = new Night("7:00PM", "1:00AM");
+            Family familyC = new FamilyC();
+            PayCalculator calcC = new PayCalculator(familyC, night3);
+            Console.WriteLine(calcC.CalculatePay());
+
+
         }
     }
 }
