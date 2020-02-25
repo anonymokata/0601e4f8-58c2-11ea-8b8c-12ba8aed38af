470cbac31bb37e1bbcc1b06de5a41280bdf6b970
someone <someone>
2020-02-25 17:19:51 -0500

    Added tests for input regex now that it is public method

---
+++ BabysitterKada/BabysitterKadaTests/TimeTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BabysitterKada.Classes;
+
+namespace BabysitterKadaTests
+{
+    [TestClass]
+    public class TimeTest
+    {
+        [TestMethod]
+        public void whenThrowExceptionIfInputIsInvalidTimeStringFormatIsPassedLettersThrowsEx()
+        {
+            try
+            {
+                Time.throwExceptionIfInputIsInvalidTimeStringFormat("abc");
+            }
+            catch(Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "Invalid time format. Please use hh:mm:am.  Ex: 6:30AM, 12:10PM");
+            }
+        }
+
+        [TestMethod]
+        public void whenThrowExceptionIfInputIsInvalidTimeStringFormatIsPassedBadTimeThrowsEx()
+        {
+            try
+            {
+                Time.throwExceptionIfInputIsInvalidTimeStringFormat("27:00AM");
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "Invalid time format. Please use hh:mm:am.  Ex: 6:30AM, 12:10PM");
+            }
+        }
+
+        [TestMethod]
+        public void whenThrowExceptionIfInputIsInvalidTimeStringFormatIsPassedImproperFormatThrowsEx()
+        {
+            try
+            {
+                Time.throwExceptionIfInputIsInvalidTimeStringFormat("1200  PM");
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "Invalid time format. Please use hh:mm:am.  Ex: 6:30AM, 12:10PM");
+            }
+        }
+    }
+}
