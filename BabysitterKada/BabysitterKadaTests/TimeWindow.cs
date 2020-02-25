f96e807e24509f464da57785bdf9e6d316c4789e
someone <someone>
2020-02-25 14:57:15 -0500

    Added test for TimeWindow

---
+++ BabysitterKada/BabysitterKadaTests/TimeWindow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BabysitterKada.Classes;
+
+namespace BabysitterKadaTests
+{
+    [TestClass]
+    public class TimeWindowTest
+    {
+        TimeWindow window = new TimeWindow(DateTime.Parse("5:00PM"), DateTime.Parse("12:00AM").AddDays(1));
+
+        [TestMethod]
+        public void whenGetHoursWorkedWithinATimeRangeHasNoHoursInRangeReturnsZero()
+        {
+            Assert.AreEqual(0, window.getHoursWorkedWithinATimeRange(DateTime.Parse("1:00PM"), DateTime.Parse("4:00PM")));
+        }
+
+        [TestMethod]
+        public void whenGetHoursWorkedWithinATimeRangeHasOnlyStartInRangeReturnsCorrectHours()
+        {
+            Assert.AreEqual(3, window.getHoursWorkedWithinATimeRange(DateTime.Parse("9:00PM"), DateTime.Parse("12:00AM").AddDays(1)));
+        }
+
+        [TestMethod]
+        public void whenGetHoursWorkedWithinATimeRangeHasOnlyEndInRangeReturnsCorrectHours()
+        {
+            Assert.AreEqual(1, window.getHoursWorkedWithinATimeRange(DateTime.Parse("4:00PM"), DateTime.Parse("6:00PM")));
+        }
+    }
+}
