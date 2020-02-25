73c292912e1ac1357eae18d9568c0d85bb7605a5
someone <someone>
2020-02-25 17:25:29 -0500

    Added unit test for helper function

--- BabysitterKada/BabysitterKadaTests/TimeTest.cs
+++ BabysitterKada/BabysitterKadaTests/TimeTest.cs
@@ -47,5 +47,19 @@ namespace BabysitterKadaTests
                 Assert.AreEqual(ex.Message, "Invalid time format. Please use hh:mm:am.  Ex: 6:30AM, 12:10PM");
             }
         }
+
+        [TestMethod]
+        public void whenParseStringToDateTimeAndAddDayIfAMisPassedAMaddsDay()
+        {
+            DateTime am = Time.parseStringToDateTimeAndAddDayIfAM("4:00AM");
+            Assert.AreEqual(DateTime.Now.Day + 1, am.Day);
+        }
+
+        [TestMethod]
+        public void whenParseStringToDateTimeAndAddDayIfAMisPassedPMdoesNotAddDay()
+        {
+            DateTime am = Time.parseStringToDateTimeAndAddDayIfAM("4:00PM");
+            Assert.AreEqual(DateTime.Now.Day, am.Day);
+        }
     }
 }
